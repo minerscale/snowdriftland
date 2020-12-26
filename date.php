3bea85367d2fbe1193187fe802c9b90518b0d815
gyroninja <gyroninja@gmail.com>
2020-12-26 07:06:09 -0800

    Added reimplementation of date.php
    From what I can tell this file needs to be in the root directory of
    the game. The game expects ActionScript name-value pairs that includes
    a key "currentdate" that is set to an integer representing the number
    of seconds since the UNIX epoch. This value is loaded using LoadVars.
    
    https://helpx.adobe.com/adobe-media-server/ssaslr/loadvars-class.html

---
+++ date.php
@@ -0,0 +1,9 @@
+// Copyright 2020 gyroninja
+// This program is licensed under the CC0 1.0 Universal license
+// https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt
+// This is a reimplementation of date.php that
+// returns a UNIX timestamp in seconds
+<?php
+echo "currentdate=" . (new DateTime())->getTimestamp()
+?>
+
